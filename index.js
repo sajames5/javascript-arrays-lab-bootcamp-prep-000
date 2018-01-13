9106fed2ddf5e88d5c83476f7c698a41f4f676bc
Stephanie <s.a.james5@gmail.com>
2018-01-13 18:42:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'] 
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift(name)
+}
+function appendKitten(name) {
+  ...kittens
+  return name
+}
